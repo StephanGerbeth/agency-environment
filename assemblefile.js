5b6e615dded4ab653700fbb1254535d6ebacb272
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2017-03-04 16:01:22 +0100

    optimized root script

--- assemblefile.js
+++ assemblefile.js
@@ -1,24 +1,24 @@
 "use strict";
 
 var template = require('lodash/template');
-var options = require('minimist')(process.argv.slice(2));
 var upath = require('upath');
-var serverConfig = JSON.parse(template(JSON.stringify(require(upath.join(process.cwd(), (options.serverConfig || './env/config/local.json')))))({'root': upath.join(process.cwd())}))[process.env.NODE_ENV];
-var tasksConfig = JSON.parse(template(JSON.stringify(require(upath.join(process.cwd(), options.configTasks))))({'destination': upath.join(serverConfig.dest), 'root': upath.join(process.cwd())}));
+var options = require('minimist')(process.argv.slice(2));
+var serverConfig = JSON.parse(template(JSON.stringify(require(upath.join(process.cwd(), options.serverConfig))))({'root': upath.join(process.cwd())}))[process.env.NODE_ENV];
+var gulpFileMap = JSON.parse(template(JSON.stringify(require(upath.join(process.cwd(), options.gulpFileMap))))({'destination': upath.join(serverConfig.dest), 'root': upath.join(process.cwd())}));
 
 var gulp = require('gulp');
 var runSequence = require('run-sequence').use(gulp);
 var livereload = require('gulp-livereload');
 
-gulp.task('clean', require('./lib/tasks/clean')('clean', tasksConfig.clean, serverConfig));
-gulp.task('copy', require('./lib/tasks/copy')('copy', tasksConfig.copy, serverConfig));
-gulp.task('fontmin', require('./lib/tasks/fontmin')('fontmin', tasksConfig.fontmin, serverConfig));
-gulp.task('handlebars', require('./lib/tasks/handlebars')('handlebars', tasksConfig.handlebars, serverConfig));
-gulp.task('postcss', require('./lib/tasks/postcss')('postcss', tasksConfig.postcss, serverConfig));
-gulp.task('purecss', require('./lib/tasks/purecss')(tasksConfig.purecss));
-gulp.task('sitemap', require('./lib/tasks/sitemap')('sitemap', tasksConfig.sitemap, serverConfig));
-gulp.task('validate', require('./lib/tasks/validate')('validate', tasksConfig.validate, serverConfig)());
-gulp.task('webpack', require('./lib/tasks/webpack')('webpack', tasksConfig.webpack, serverConfig)());
+gulp.task('clean', require('./lib/tasks/clean')('clean', gulpFileMap.clean, serverConfig));
+gulp.task('copy', require('./lib/tasks/copy')('copy', gulpFileMap.copy, serverConfig));
+gulp.task('fontmin', require('./lib/tasks/fontmin')('fontmin', gulpFileMap.fontmin, serverConfig));
+gulp.task('handlebars', require('./lib/tasks/handlebars')('handlebars', gulpFileMap.handlebars, serverConfig));
+gulp.task('postcss', require('./lib/tasks/postcss')('postcss', gulpFileMap.postcss, serverConfig));
+gulp.task('purecss', require('./lib/tasks/purecss')(gulpFileMap.purecss));
+gulp.task('sitemap', require('./lib/tasks/sitemap')('sitemap', gulpFileMap.sitemap, serverConfig));
+gulp.task('validate', require('./lib/tasks/validate')('validate', gulpFileMap.validate, serverConfig)());
+gulp.task('webpack', require('./lib/tasks/webpack')('webpack', gulpFileMap.webpack, serverConfig)());
 gulp.task('watch', function(cb) {
     if(serverConfig.livereload) {
         livereload.listen({
@@ -27,7 +27,7 @@ gulp.task('watch', function(cb) {
     }
     cb();
 });
-gulp.task('zip-compress', require('./lib/tasks/zip-compress')('zip-compress', tasksConfig.zipcompress, serverConfig));
+gulp.task('zip-compress', require('./lib/tasks/zip-compress')('zip-compress', gulpFileMap.zipcompress, serverConfig));
 
 gulp.task('build', function(callback) {
     runSequence('prebuild', 'webpack:app', ['validate', 'sitemap'], 'zip-compress:default', callback);
